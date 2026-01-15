a2fa0026ecf57d13e0df2066517b1353d4eff092
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2026-01-15 19:31:22 -0300

    ahora que se termina de definir que el base_repository esta en el framework, entonces podemos hacer override del metodo create o cualquier otro cuando hace falta, en este caso la unicidad del nombre+deleted_at

--- app/core/database/models/genres.py
+++ app/core/database/models/genres.py
@@ -16,11 +16,17 @@ from pegasus_framework.db.models.mixins import AuditMixin
 class Genre(AuditMixin, Base):
     __tablename__ = "genres"
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False, unique=True, index=True)
+    name = Column(String(50), nullable=False)
 
     movies = relationship("Movie", back_populates="genre")
 
     __table_args__ = (
+        Index(
+            "uq_genres_name_deleted_at",
+            "name",
+            "deleted_at",
+            unique=True,
+        ),        
         Index("ix_genres_active", "habilited", "deleted_at"),
         Index("ix_genres_name", "name"),
     )    
--- app/core/database/repositories/genre_repository.py
+++ app/core/database/repositories/genre_repository.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 from pegasus_framework.db.repositories.base_repository import BaseRepository
 from app.core.database.models.genres import Genre
-
+from sqlalchemy.exc import IntegrityError
+from pegasus_framework.core.exceptions.domain.duplicate_entry import DuplicateEntityError
 
 
 
@@ -9,3 +10,19 @@ from app.core.database.models.genres import Genre
 class GenreRepository(BaseRepository[Genre]):
     def __init__(self, session: Session):
         super().__init__(Genre, session)
+
+    def create(self, data: dict) -> Genre:
+        try:
+            return super().create(data)
+
+        except IntegrityError as e:
+            self.session.rollback()
+
+            if "duplicate entry" in str(e.orig).lower():
+                raise DuplicateEntityError(
+                    entity="Genre",
+                    field="name",
+                    value=data.get("name"),
+                ) from e
+
+            raise
