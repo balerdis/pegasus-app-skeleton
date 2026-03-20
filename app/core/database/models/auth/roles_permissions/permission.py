898ef63d792400f0008572c12e3bae2885fc8a0d
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2026-03-20 14:22:56 -0300

    resuelvo temas de referencia circular que solo le pasaban al linter, por eso utilizo TYPE_CHECKING

--- app/core/database/models/auth/roles_permissions/permission.py
+++ app/core/database/models/auth/roles_permissions/permission.py
@@ -1,11 +1,14 @@
 # app/core/database/models/auth/roles_permissions/permission.py
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from typing import List
+from typing import List, TYPE_CHECKING
 
 from app.core.database.models.base import Base
 from pegasus_framework.db.models.auth.roles_permissions.permission_base import BasePermission
 from pegasus_framework.db.models.mixins import AuditMixin
+
+if TYPE_CHECKING:
+    from app.core.database.models.auth.roles_permissions.role import Role
 
 class Permission(AuditMixin, Base, BasePermission):
     __tablename__ = "permissions"
@@ -14,7 +17,7 @@ class Permission(AuditMixin, Base, BasePermission):
     code: Mapped[str] = mapped_column(String(255), unique=True)
     description: Mapped[str | None] = mapped_column(String(255))
 
-    roles: Mapped[List["Role"]] = relationship( # type: ignore
+    roles: Mapped[List["Role"]] = relationship(
         "Role",
         secondary="roles_permissions",
         back_populates="permissions"
--- app/core/database/models/auth/roles_permissions/role.py
+++ app/core/database/models/auth/roles_permissions/role.py
@@ -1,12 +1,16 @@
 # app/core/database/models/auth/roles_permissions/role.py
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from typing import List
+from typing import List, TYPE_CHECKING
 
 from app.core.database.models.base import Base
 
 from pegasus_framework.db.models.auth.roles_permissions.rol_base import BaseRole
 from pegasus_framework.db.models.mixins import AuditMixin
+
+if TYPE_CHECKING:
+    from app.core.database.models.auth.roles_permissions.permission import Permission
+    from app.core.database.models.users import User
 
 class Role(AuditMixin, Base, BaseRole):
     __tablename__ = "roles"
@@ -15,9 +19,9 @@ class Role(AuditMixin, Base, BaseRole):
     code: Mapped[str] = mapped_column(String(255), unique=True)
     description: Mapped[str | None] = mapped_column(String(255))
 
-    permissions: Mapped[List["Permission"]] = relationship( # type: ignore
+    permissions: Mapped[List["Permission"]] = relationship( 
         "Permission", secondary="roles_permissions", back_populates="roles"
     )
-    users: Mapped[List["User"]] = relationship( # type: ignore
+    users: Mapped[List["User"]] = relationship( 
         "User", secondary="users_roles", back_populates="roles"
     )
